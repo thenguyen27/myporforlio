8bdf78b6e53542de748d6603ddd26045a48918a9
Nguyen <t.thenguyen27@gmail.com>
2025-07-31 01:18:30 +0700

    add/Push code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "portfolio"; // ✨ thay bằng tên repo của bạn
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true,
   },
