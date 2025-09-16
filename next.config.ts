f437d1c94a78ad36bddc6232e1f80ca510819b52
Nguyen <t.thenguyen27@gmail.com>
2025-09-17 00:06:08 +0700

    push fix deploy vercel

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,12 @@
 import type { NextConfig } from "next";
 
-const repoName = "myporforlio";
-
 const nextConfig: NextConfig = {
-  output: "export",
-
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`, // 👈 ép Next.js load assets từ /resources
 
   trailingSlash: false,
-
   images: {
-    unoptimized: true,
   },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
 
 export default nextConfig;
