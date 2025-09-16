f24774ce2ba3209e6d27c912429aa0e4c9a407c5
Nguyen <t.thenguyen27@gmail.com>
2025-09-16 22:48:38 +0700

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pacifico } from "next/font/google";
-import "./app/globals.css";
+import "./globals.css";
 
 const pacifico = Pacifico({
   weight: '400',
